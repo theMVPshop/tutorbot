c6c27012be5e8348702b6fdd85fc081b4d195b01
mannybennett <mannybennett99@gmail.com>
2023-11-28 22:17:01 -0500

    Add streaming functionality

--- src/Chatbot.js
+++ src/Chatbot.js
@@ -1,32 +1,26 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, useMemo, useCallback } from "react";
 import "./App.css";
 import DevLogo from "../src/assets/DevLogo.png";
-// import { formatLog, responseAPI } from "./api";
-import OpenAI from 'openai';
 import Markdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
+import Chat from "./models/Chat"
+
+const CHAT_CONFIG = {
+  model: "gpt-4",
+  purpose: "You are an expert programmer and tutor, and your goal is to guide and support students of Austin Coding Academy in their journey to learn various programming languages and technologies. They will be beginners, some with no knowledge of coding or programming at all; keep this in mind as you reply to them. Provide sequential guidance and resources for learning web development languages and technologies taught at Austin Coding Academy. Offer structured learning paths for the relevant languages or frameworks. Suggest supplementary resources such as tutorials, articles, and projects for practical learning. Respond to specific queries related to coding concepts, syntax, and problem-solving strategies. Offer explanations, code snippets, and examples to aid in understanding complex topics. Assist students in their coding projects by offering suggestions, debugging tips, and best practices. Provide guidance on project structuring, debugging techniques, and version control."
+}
+
 const ChatBot = () => {
-  // const [log, setLog] = useState([]);
-  const [userInput, setUserInput] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [prompt, setPrompt] = useState('');
+  const [stream, setStream] = useState('');
+  const [chatHistory, setChatHistory] = useState([]);
 
-  const [messages, setMessages] = useState([]);
-
-  // const [assistantReply, setAssistantReply] = useState("");
-
-  const handleUserInput = (e) => {
-    if (e.key === "Enter" && !e.shiftKey) {
-      e.preventDefault();
-      chat(userInput);
-    } else {
-      setUserInput(e.target.value);
-    }
-  };
-
-  const logEndRef = useRef(null);
+  const chatEndRef = useRef(null);
   const autoScroll = () => {
-    logEndRef.current?.scrollIntoView({ behavior: "smooth", block: "end" });
+    chatEndRef.current?.scrollIntoView({ behavior: "smooth", block: "end" });
   };
 
   // expandInput func has changed to reduce the size of chatbox when conversation getting longer
@@ -42,99 +36,62 @@ const ChatBot = () => {
     e.target.style.height = newHeight;
   };
 
-  // const handleSendMessage = () => {
-  //   responseAPI(userInput, setLog)
-
-  //   setMessages([
-  //     ...messages,
-  //     userInput,
-  //     formatLog(log),
-  //   ]);
-
-  //   // Clear the user input field
-  //   setUserInput("")
-  // };
-
-  // This is updated code for onKeyDown (Enter) to Submit
-  // const handleSendMessage = () => {
-  //   if (userInput.trim() === "") {
-  //     return; // Don't send empty messages
-  //   }
-
-  //   const userMessage = { role: "user", content: userInput };
-  //   const assistantMessage = "This is a hardcoded reply"; // You can replace this with your actual assistant reply logic
-
-  //   const updatedMessages = [
-  //     ...messages,
-  //     userMessage,
-  //     { role: "assistant", content: assistantMessage },
-  //   ];
-
-  //   setMessages(updatedMessages);
-  //   setAssistantReply(assistantMessage);
-
-  //   // Reset textarea height after sending the message
-  //   const textarea = document.querySelector(".user__input textarea");
-  //   if (textarea) {
-  //     textarea.style.height = "auto";
-  //   }
-
-  //   // Clear the user input field
-  //   setUserInput("");
-  // };
-
-  //Configure API ***
-  const openai = new OpenAI({
-    apiKey: process.env.REACT_APP_KEY,
-    dangerouslyAllowBrowser: true,
-  });
-  
-  //New Function ***
-  const chat = async (userInput) => {
-    if (userInput.trim() === "") {
-      return; // Don't send empty messages
+  const handleChange = (e) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      handleSubmit();
+    } else {
+      setPrompt(e.target.value);
     }
+  };
 
-    let msgs = messages;
-    msgs.push({ role: "user", content: userInput });
-    setMessages(msgs);
+  const callback = useCallback(chunk => setStream(currStream => currStream + chunk), []);
 
+  const chat = useMemo(() => {
+    const newChat = new Chat(CHAT_CONFIG, callback);
+    newChat.setApiKey(process.env.REACT_APP_KEY);
+    return newChat;
+  }, [callback]);
+
+  useEffect(() => {
+    if (stream) {
+      const updatedHistory = [...chatHistory];
+      updatedHistory[updatedHistory.length - 1] = { role: 'assistant', content: stream };
+      setChatHistory(updatedHistory);
+      console.log(stream);
+    }
+  }, [stream]);
+
+  const handleSubmit = async () => {
+
+    if (prompt.trim() === "") {
+        return; // Don't send empty messages
+    };
+
+    setIsLoading(true);
+    setChatHistory([...chatHistory, { role: 'user', content: prompt }, { role: '', content: '' }]);
+    
     // Reset textarea height after sending the message
     const textarea = document.querySelector(".user__input textarea");
     if (textarea) {
       textarea.style.height = "auto";
     }
+
     // Clear the user input field
-    setUserInput("");
-
-    const completion = await openai.chat.completions.create({
-      model: "gpt-4-1106-preview",
-      messages: [
-        {
-          role: "system",
-          content:
-            "You are an expert programmer and tutor, and your goal is to guide and support students of Austin Coding Academy in their journey to learn various programming languages and technologies. They will be beginners, some with no knowledge of coding or programming at all; keep this in mind as you reply to them. Provide sequential guidance and resources for learning web development languages and technologies taught at Austin Coding Academy. Offer structured learning paths for the relevant languages or frameworks. Suggest supplementary resources such as tutorials, articles, and projects for practical learning. Respond to specific queries related to coding concepts, syntax, and problem-solving strategies. Offer explanations, code snippets, and examples to aid in understanding complex topics. Assist students in their coding projects by offering suggestions, debugging tips, and best practices. Provide guidance on project structuring, debugging techniques, and version control.",
-        },
-        ...messages,
-      ],
-      stream: true,
-    });
-
-    let newMessages = ''
-
-    for await (const chunk of completion) {
-      console.log(chunk.choices[0].delta.content);
-      if (chunk.choices[0].delta.content) {
-        newMessages += chunk.choices[0].delta.content;
-      }
+    setPrompt("");
+    try {
+      await chat.createResponse(prompt);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setStream("");
+      setIsLoading(false);
     }
-
-    setMessages((prevMessages) => [...prevMessages, {role: "assistant", content: newMessages}]);
   };
 
   useEffect(() => {
     autoScroll();
-  }, [messages]);
+  }, [chatHistory]);
 
   return (
     <div className="chatbot__main">
@@ -145,20 +102,12 @@ const ChatBot = () => {
         </div>
       </div>
       <div className="section__padding">
-        {/* <div className="chatbox__cont ">
-          {messages.map((message, index) => (
-            <div key={index} className="chatbox">
-              {message}
-            </div>
-          ))}
-          <div ref={logEndRef} />
-        </div> */}
 
         <div
           className="chatbox__cont"
           style={{ maxHeight: `calc(88vh - ${textareaHeight})` }}
         >
-          {messages.map((message, index) => (
+          {chatHistory.map((message, index) => (
             <div
               key={index}
               className={`message-container ${
@@ -202,19 +151,20 @@ const ChatBot = () => {
               )}
             </div>
           ))}
+          <div ref={chatEndRef}></div>
         </div>
 
         <div className="user__input">
           <textarea
             style={{ height: textareaHeight }}
-            value={userInput}
-            onChange={handleUserInput}
+            value={prompt}
+            onChange={handleChange}
             onInput={expandInput}
             placeholder="Type a message..."
             rows="1"
-            onKeyDown={handleUserInput}
+            onKeyDown={handleChange}
           />
-          <button onClick={() => chat(userInput)}>Send</button>
+          <button disabled={isLoading} onClick={handleSubmit}>Send</button>
         </div>
       </div>
     </div>
